4d672300e5073fb2f3e9e1c903b043faeceb1f58
Hoova T <htapplied@yahoo.com>
2023-04-04 14:54:31 +0000

    Loops

---
+++ AddingNumbers.py
@@ -0,0 +1,11 @@
+#Adding Numbers
+
+num1 = 1
+num2 = 2
+num3 = 3
+
+#Add 3 numbers
+sum = num1 + num2 + num3
+
+#Display sum
+print('The sum of {0} {1} and {2} is {3}'.format(num1, num2, num3, sum))
---
+++ Loops.py
@@ -0,0 +1,5 @@
+counter = 1 
+while counter <= 66:
+    if counter % 6 == 0:
+        print(counter)
+    counter += 1
