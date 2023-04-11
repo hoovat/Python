3e91de85bac9aa528ebd8b6a962748f8890966fb
Hoova T <htapplied@yahoo.com>
2023-04-11 04:08:15 +0000

    Adding Numbers

---
+++ AddingNumbers.py
@@ -0,0 +1,11 @@
+#Numbers to be added
+
+num1 = 5
+num2 = 10
+num3 = 15
+
+#Add Numbers
+total = num1 + num2 + num3
+
+#Total
+print('The total of {0} {1} and {2} is {3}'.format(num1, num2, num3, total))
